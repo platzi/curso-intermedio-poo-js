67d2a6493a0e174a96493589e87146ea72399035
Juan David Castro <me@juandc.co>
2021-06-21 17:08:45 -0500

    Clase 07 en orden de grabación

--- main.js
+++ main.js
@@ -5,12 +5,10 @@ const obj1 = {
     d: "d",
     e: "e",
   },
+  editA() {
+    this.a = "AAAAAA";
+  }
 };
 
-const obj2 = {};
-for (prop in obj1) {
-  obj2[prop] = obj1[prop];
-}
-
-const obj3 = Object.assign({}, obj1);
-const obj4 = Object.create(obj1);
+const stringifiedComplexObj = JSON.stringify(obj1);
+const obj2 = JSON.parse(stringifiedComplexObj);
