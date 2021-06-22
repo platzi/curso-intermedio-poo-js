50d510c26ee3db18d7fbcafac3b12755ed00133b
Juan David Castro <me@juandc.co>
2021-06-22 16:39:33 -0500

    Clase 13 en orden de grabación

--- main.js
+++ main.js
@@ -78,25 +78,36 @@ function createStudent({
       instagram,
       facebook,
     },
-    readName() {
+    get name() {
       return private["_name"];
     },
-    changeName(newName) {
-      private["_name"] = newName;
+    set name(newName) {
+      if (newName.length != 0) {
+        private["_name"] = newName;
+      } else {
+        console.warn("Tu nombre debe tener al menos 1 caracter");
+      }
     },
+    // readName() {
+    //   return private["_name"];
+    // },
+    // changeName(newName) {
+    //   private["_name"] = newName;
+    // },
   };
 
-  Object.defineProperty(public, "readName", {
-    writable: false,
-    configurable: false,
-  });
-  Object.defineProperty(public, "changeName", {
-    writable: false,
-    configurable: false,
-  });
+  // Object.defineProperty(public, "readName", {
+  //   writable: false,
+  //   configurable: false,
+  // });
+  // Object.defineProperty(public, "changeName", {
+  //   writable: false,
+  //   configurable: false,
+  // });
 
   return public;
 }
 
 const juan = createStudent({ email: "juanito@frijoles.co", name: "Juanito" });
+
 
