80adcfc7c1b4add4882dc2657fe4127eb13708a4
Juan David Castro <me@juandc.co>
2021-06-22 17:41:39 -0500

    Clase 17 en orden de grabación

--- main.js
+++ main.js
@@ -93,68 +93,28 @@ function Student({
     facebook,
   };
 
-  if (isArray(learningPaths)) {
-    this.learningPaths = [];
-    
-    for (learningPathIndex in learningPaths) {
-      if (learningPaths[learningPathIndex] instanceof LearningPath) {
-        this.learningPaths.push(learningPaths[learningPathIndex]);
+  const private = {
+    "_learningPaths": [],
+  };
+
+  Object.defineProperty(this, "learningPaths", {
+    get() {
+      return private["_learningPaths"];
+    },
+    set(newLp) {
+      if (newLp instanceof LearningPath) {
+        private["_learningPaths"].push(newLp);
+      } else {
+        console.warn("Alguno de los LPs no es una instancia dell prototipo LearningPath");
       }
-    }
+    },
+  });
+
+  for (learningPathIndex in learningPaths) {
+    this.learningPaths = learningPaths[learningPathIndex];
   }
-  
-  
-  
-
-  // const private = {
-  //   "_name": name,
-  //   "_learningPaths": learningPaths,
-  // };
-
-  // const public = {
-  //   email,
-  //   age,
-  //   approvedCourses,
-  //   socialMedia: {
-  //     twitter,
-  //     instagram,
-  //     facebook,
-  //   },
-  //   get name() {
-  //     return private["_name"];
-  //   },
-  //   set name(newName) {
-  //     if (newName.length != 0) {
-  //       private["_name"] = newName;
-  //     } else {
-  //       console.warn("Tu nombre debe tener al menos 1 caracter");
-  //     }
-  //   },
-  //   get learningPaths() {
-  //     return private["_learningPaths"];
-  //   },
-  //   set learningPaths(newLP) {
-  //     if (!newLP.name) {
-  //       console.warn("Tu LP no tiene la propiedad name");
-  //       return;
-  //     }
-
-  //     if (!newLP.courses) {
-  //       console.warn("Tu LP no tiene courses");
-  //       return;
-  //     }
-
-  //     if (!isArray(newLP.courses)) {
-  //       console.warn("Tu LP no es una lista (*de cursos)");
-  //       return;
-  //     }
-      
-  //     private["_learningPaths"].push(newLP);
-  //   },
-  // };
-
-  // return public;
 }
+
 
 const escuelaWeb = new LearningPath({ name: "Escuela de WebDev" });
 const escuelaData = new LearningPath({ name: "Escuela de Data Science" });
